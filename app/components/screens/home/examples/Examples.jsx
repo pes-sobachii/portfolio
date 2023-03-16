6d7b6f374da664edc3e329f731c4d26fce86e999
Alex Solohub <le03grande@gmail.com>
2023-03-16 11:53:50 +0200

    project added

--- app/components/screens/home/examples/Examples.jsx
+++ app/components/screens/home/examples/Examples.jsx
@@ -7,6 +7,7 @@ import movieImg from '../../../../assets/movieapp.jpg'
 import storeImg from '../../../../assets/bookstore.jpg'
 import socialImg from '../../../../assets/social.jpg'
 import portfolioImg from '../../../../assets/portfolio.jpg'
+import newTodoImg from '../../../../assets/photo_2023-03-16_11-50-51.jpg'
 import Image from "next/image";
 import styles from './Examples.module.scss'
 
@@ -14,21 +15,27 @@ const Examples = () => {
     return (
         <section className={styles.section}>
             <h2 className={styles.title}>Experience</h2>
+
             <div>
                 <div className={styles.row}>
                     <div className={styles.image}>
-                        <Image src={toDoImg} alt='To Do App'/>
+                        <Image src={newTodoImg} alt='Todo app'/>
                     </div>
                     <div className={styles.info}>
-                        <h3 className={styles.name}>To Do App</h3>
-                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <h3 className={styles.name}>Todo App</h3>
+                        <p className={styles.stack}>Stack: Next.js, React-Query, Styled-Components, Node.js, Express, MySQL</p>
                         <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/todo-list-pure-js">
+                            <a target='_blank' href="https://github.com/pes-sobachii/todo_frontend">
                                 <button
-                                    className={styles.repo}>Repository
+                                    className={styles.repo}>Frontend Repo
                                 </button>
                             </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/todo-list-pure-js/">
+                            <a target='_blank' href="https://github.com/pes-sobachii/todo_backend">
+                                <button
+                                    className={styles.repo}>Backend Repo
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://todo-frontend-sable.vercel.app/register">
                                 <button
                                     className={styles.deploy}>Deployment
                                 </button>
@@ -38,104 +45,15 @@ const Examples = () => {
                 </div>
                 <div className={styles.row + ' ' + styles.reversed}>
                     <div className={styles.image}>
-                        <Image src={quizImg} alt='Quiz App'/>
+                        <Image src={portfolioImg} alt='Portfolio'/>
                     </div>
                     <div className={styles.info}>
-                        <h3 className={styles.name}>Quiz App</h3>
-                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <h3 className={styles.name}>Portfolio (this page)</h3>
+                        <p className={styles.stack}>Stack: Next.js, Tailwind, SCSS</p>
                         <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/quiz-app-js">
+                            <a target='_blank' href="https://github.com/pes-sobachii/portfolio">
                                 <button
                                     className={styles.repo}>Repository
-                                </button>
-                            </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/quiz-app-js/">
-                                <button
-                                    className={styles.deploy}>Deployment
-                                </button>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.row}>
-                    <div className={styles.image}>
-                        <Image src={tabataImg} alt='Tabata timer'/>
-                    </div>
-                    <div className={styles.info}>
-                        <h3 className={styles.name}>Tabata timer</h3>
-                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
-                        <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/tabata-timer-js">
-                                <button
-                                    className={styles.repo}>Repository
-                                </button>
-                            </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/tabata-timer-js/">
-                                <button
-                                    className={styles.deploy}>Deployment
-                                </button>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.row + ' ' + styles.reversed}>
-                    <div className={styles.image}>
-                        <Image src={ghsearchImg} alt='GitHub Search'/>
-                    </div>
-                    <div className={styles.info}>
-                        <h3 className={styles.name}>GitHub Search</h3>
-                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
-                        <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/ghsearch">
-                                <button
-                                    className={styles.repo}>Repository
-                                </button>
-                            </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/ghsearch/">
-                                <button
-                                    className={styles.deploy}>Deployment
-                                </button>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.row}>
-                    <div className={styles.image}>
-                        <Image src={movieImg} alt='Movie App'/>
-                    </div>
-                    <div className={styles.info}>
-                        <h3 className={styles.name}>Movie App</h3>
-                        <p className={styles.stack}>Stack: React, Redux Toolkit, SCSS, TypeScript</p>
-                        <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/movie-app-ts">
-                                <button
-                                    className={styles.repo}>Repository
-                                </button>
-                            </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/movie-app-ts/">
-                                <button
-                                    className={styles.deploy}>Deployment
-                                </button>
-                            </a>
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.row + ' ' + styles.reversed}>
-                    <div className={styles.image}>
-                        <Image src={storeImg} alt='Book Store App'/>
-                    </div>
-                    <div className={styles.info}>
-                        <h3 className={styles.name}>Book Store App</h3>
-                        <p className={styles.stack}>Stack: React, Redux Toolkit, Bootstrap, TypeScript</p>
-                        <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/book-store-ts">
-                                <button
-                                    className={styles.repo}>Repository
-                                </button>
-                            </a>
-                            <a target='_blank' href="https://pes-sobachii.github.io/book-store-ts/">
-                                <button
-                                    className={styles.deploy}>Deployment
                                 </button>
                             </a>
                         </p>
@@ -169,20 +87,131 @@ const Examples = () => {
                 </div>
                 <div className={styles.row + ' ' + styles.reversed}>
                     <div className={styles.image}>
-                        <Image src={portfolioImg} alt='Portfolio'/>
+                        <Image src={storeImg} alt='Book Store App'/>
                     </div>
                     <div className={styles.info}>
-                        <h3 className={styles.name}>Portfolio (this page)</h3>
-                        <p className={styles.stack}>Stack: Next.js, Tailwind, SCSS</p>
+                        <h3 className={styles.name}>Book Store App</h3>
+                        <p className={styles.stack}>Stack: React, Redux Toolkit, Bootstrap, TypeScript</p>
                         <p className={styles.buttons}>
-                            <a target='_blank' href="https://github.com/pes-sobachii/portfolio">
+                            <a target='_blank' href="https://github.com/pes-sobachii/book-store-ts">
                                 <button
                                     className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/book-store-ts/">
+                                <button
+                                    className={styles.deploy}>Deployment
                                 </button>
                             </a>
                         </p>
                     </div>
                 </div>
+                <div className={styles.row}>
+                    <div className={styles.image}>
+                        <Image src={movieImg} alt='Movie App'/>
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.name}>Movie App</h3>
+                        <p className={styles.stack}>Stack: React, Redux Toolkit, SCSS, TypeScript</p>
+                        <p className={styles.buttons}>
+                            <a target='_blank' href="https://github.com/pes-sobachii/movie-app-ts">
+                                <button
+                                    className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/movie-app-ts/">
+                                <button
+                                    className={styles.deploy}>Deployment
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.row + ' ' + styles.reversed}>
+                    <div className={styles.image}>
+                        <Image src={ghsearchImg} alt='GitHub Search'/>
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.name}>GitHub Search</h3>
+                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <p className={styles.buttons}>
+                            <a target='_blank' href="https://github.com/pes-sobachii/ghsearch">
+                                <button
+                                    className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/ghsearch/">
+                                <button
+                                    className={styles.deploy}>Deployment
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.image}>
+                        <Image src={tabataImg} alt='Tabata timer'/>
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.name}>Tabata timer</h3>
+                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <p className={styles.buttons}>
+                            <a target='_blank' href="https://github.com/pes-sobachii/tabata-timer-js">
+                                <button
+                                    className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/tabata-timer-js/">
+                                <button
+                                    className={styles.deploy}>Deployment
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.row + ' ' + styles.reversed}>
+                    <div className={styles.image}>
+                        <Image src={quizImg} alt='Quiz App'/>
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.name}>Quiz App</h3>
+                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <p className={styles.buttons}>
+                            <a target='_blank' href="https://github.com/pes-sobachii/quiz-app-js">
+                                <button
+                                    className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/quiz-app-js/">
+                                <button
+                                    className={styles.deploy}>Deployment
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.image}>
+                        <Image src={toDoImg} alt='To Do App'/>
+                    </div>
+                    <div className={styles.info}>
+                        <h3 className={styles.name}>To Do App</h3>
+                        <p className={styles.stack}>Stack: HTML, CSS, JS</p>
+                        <p className={styles.buttons}>
+                            <a target='_blank' href="https://github.com/pes-sobachii/todo-list-pure-js">
+                                <button
+                                    className={styles.repo}>Repository
+                                </button>
+                            </a>
+                            <a target='_blank' href="https://pes-sobachii.github.io/todo-list-pure-js/">
+                                <button
+                                    className={styles.deploy}>Deployment
+                                </button>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
             </div>
         </section>
     );
